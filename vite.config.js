e9bd8d3ca7ba46695d61ff1f95eb9bd1d3deb1f8
yaman1337 <yaman.sarabariya@gmail.com>
2024-10-07 23:22:37 +0545

    fix vite build option

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        main: resolve(__dirname, 'data.html'),
+        data: resolve(__dirname, 'data.html'),
         flyby: resolve(__dirname, 'flyby.html'),
       },
     },
